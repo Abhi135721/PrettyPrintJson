484fd5f676492d8c539d32fd476ab20a889bc68c
Abhi135721 <abhi.amma18@gmail.com>
2018-03-05 16:36:24 +0530

    Done indent and compact

--- PrettyPrint/sampleCheck.java
+++ PrettyPrint/sampleCheck.java
@@ -15,20 +15,40 @@ import org.json.simple.parser.JSONParser;
 public class sampleCheck {
 	public static void main(String [] args){
 		JSONParser parser = new JSONParser();
+		String indent;
+		indent = "  ";
+		boolean compact = false;
+		if(compact)
+			indent = "";
 		try
         {
 			Object object = parser.parse(new FileReader("src/sample.json"));
-			System.out.println(object.getClass().getName());
             if(object.getClass().getName().equals("org.json.simple.JSONArray")){
             	JSONArray A = (JSONArray)object;
-            	String indent = "    ";
-            	System.out.println("[");
+            	//System.out.println(A);
+            	if(compact)
+            		System.out.print("[");
+            	else
+            		System.out.println("[");
+            	
             	for(int i = 0 ; i < A.size() ; i++){
-            		pretty_object((JSONObject)A.get(i));
-            		if(i != A.size() - 1)
-            			System.out.print(",");
+            		System.out.print(indent);
+            		pretty_object((JSONObject)A.get(i) , indent , 1 , compact);
+            		if(i != A.size() - 1){
+            			if(compact)
+                    		System.out.print(",");
+            			else
+            			System.out.println(",");
+            		}
+            		else{
+            			if(!compact)
+            				System.out.println();
+            		}
             	}
-            	System.out.println("]");
+            	if(compact)
+            		System.out.print("]");
+            	else
+            		System.out.println("]");
             }
             
 			else{
@@ -36,7 +56,7 @@ public class sampleCheck {
 	            //convert Object to JSONObject
 	            JSONObject jsonObject = (JSONObject)object;
 	            
-	            pretty_object(jsonObject);
+	            pretty_object(jsonObject , indent , 0 , compact);
             }
         }
 		catch(FileNotFoundException fe){
@@ -55,37 +75,64 @@ public class sampleCheck {
 			System.out.println("There is no such file to write");
 		}
 	}
-	public static void pretty_object(JSONObject jsonObject){
+	public static void pretty_object(JSONObject jsonObject, String indent , int check_type , boolean compact){
 		Set<String> keys = jsonObject.keySet();
         String s = jsonObject.toJSONString();
-        String indent = "    ";
-        System.out.println('{');
+        if(compact)
+    		System.out.print("{");
+        else
+        	System.out.println('{');
         int num_keys = keys.size();
         int no_key = 0;
+        String x= indent;
+        if(check_type == 1)
+        	x = indent + indent;
         for(String i : keys){
         	if(jsonObject.get(i).getClass().getName().equals("org.json.simple.JSONArray")){
-        		System.out.print(indent + "\"" + i + "\"" + " : " );
-        		printArray((JSONArray)jsonObject.get(i) , indent);
+        		if(compact)
+        			System.out.print(x + "\"" + i + "\"" + ":" );
+        		else
+        			System.out.print(x + "\"" + i + "\"" + " : " );
+        		printArray((JSONArray)jsonObject.get(i) , x , compact);
         	}
         	else if (jsonObject.get(i).getClass().getName().equals("java.lang.String")){
-        		System.out.print(indent + "\"" + i + "\"" + " : " +"\"" +  jsonObject.get(i) + "\"" );
+        		if(compact)
+        			System.out.print(x + "\"" + i + "\"" + ":" +"\"" +  jsonObject.get(i) + "\"" );
+        		else
+        			System.out.print(x + "\"" + i + "\"" + " : " +"\"" +  jsonObject.get(i) + "\"" );
         	}
         	else{
-        		System.out.print(indent + "\"" + i + "\"" + " : " +  jsonObject.get(i));
+        		if(compact)
+        			System.out.print(x + "\"" + i + "\"" + ":" +  jsonObject.get(i));
+        		else
+        			System.out.print(x + "\"" + i + "\"" + " : " +  jsonObject.get(i));
         	}
         	no_key++;
-        	if(no_key != num_keys)
-        		System.out.println(",");
+        	if(no_key != num_keys){
+        		if(compact)
+            		System.out.print(",");
+        		else
+        			System.out.println(",");
+        	}
         	else
-        		System.out.println();
+        		if(!compact){
+        			System.out.println();
+        		}
         }
-        System.out.println('}');
+        if(check_type == 0)
+        	System.out.print('}');
+        else
+        	System.out.print(indent + "}");
         //System.out.println(jsonObject);
 	}
-	public static void printArray(JSONArray A, String indent){
-		System.out.print(" [");
+	public static void printArray(JSONArray A, String indent , boolean compact){
+		if(compact)
+			System.out.print("[");
+		else
+			System.out.print(" [");
 		for(int i = 0 ; i < A.size() ; i++){
-			System.out.println();
+			if(!compact)
+				System.out.println();
 			if(A.get(i).getClass().getName().equals("java.lang.String")){
 				A.set(i, "\"" + A.get(i) + "\"");
 			}
@@ -94,7 +141,8 @@ public class sampleCheck {
 			else
 				System.out.print(indent + indent + A.get(i));
 		}
-		System.out.println();
+		if(!compact)
+			System.out.println();
 		System.out.print(indent + "]");
 	}
 }
